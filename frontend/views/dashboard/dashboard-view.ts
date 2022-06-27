b22b03ba5a4540550762b9a59044fd8e22885f3e
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2022-06-27 22:32:38 +0200

    Creating a dashboard with a chart

--- frontend/views/dashboard/dashboard-view.ts
+++ frontend/views/dashboard/dashboard-view.ts
@@ -1,6 +1,9 @@
 import { html } from "lit";
 import { customElement } from "lit/decorators.js";
 import { View } from "../view";
+import '@vaadin/charts';
+import '@vaadin/charts/src/vaadin-chart-series';
+import { dashboardViewStore } from "./dashboard-view-store";
 
 @customElement('dashboard-view')
 export class DashboardView extends View {
@@ -10,6 +13,24 @@ export class DashboardView extends View {
   }
 
   render() {
-    return html`<h1>Dashboard View</h1>`
+    return html`
+      <div class="text-xl mb-xl">
+        ${dashboardViewStore.contactCount} contacts
+      </div>
+      
+      ${this.getCompanyStats()}
+      `
+  }
+
+  getCompanyStats() {
+    if(dashboardViewStore.companyStats.length === 0) {
+      return html`<p>Loading stats...</p>`
+    } else {
+      return html`
+      <vaadin-chart type="pie">
+        <vaadin-chart-series .values=${dashboardViewStore.companyStats}></vaadin-chart-series>
+      </vaadin-chart>
+      `
+    }
   }
 }
